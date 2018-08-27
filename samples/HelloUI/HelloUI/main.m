9581828def10655e67a236e560be599b04db973d
Jan Joneš <jjones@outlook.cz>
2018-08-27 22:40:17 +0200

    Exported `main` from sample `HelloUI`.

--- samples/HelloUI/HelloUI/main.m
+++ samples/HelloUI/HelloUI/main.m
@@ -1,6 +1,11 @@
 //
 #import <UIKit/UIKit.h>
 
+// [port] CHANGED: We export this entrypoint to call it manually from our
+// [port] sample `HelloUIWrapper`.
+#if defined(OBJC_PORT)
+__declspec(dllexport)
+#endif
 int main(int argc, char* argv[])
 {
     UIApplicationMain(0, NULL, nil, @"HelloUIApp");
